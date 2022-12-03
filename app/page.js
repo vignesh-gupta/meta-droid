069fa7fe61dd60d3ac7534996371a3eac2031309
Vignesh Gupta <vighneshgupta32@gmail.com>
2022-12-03 20:38:21 +0530

    added nav

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div>
+  <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
     <About />
