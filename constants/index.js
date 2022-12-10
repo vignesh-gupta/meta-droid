888961562049f9f64d1800982e2a4a20780b85d6
Vignesh Gupta <vighneshgupta32@gmail.com>
2022-12-10 16:48:02 +0530

    added world section

--- constants/index.js
+++ constants/index.js
@@ -28,7 +28,7 @@ export const exploreWorlds = [
 
 export const startingFeatures = [
   'Find a world that suits you and you want to enter',
-  'Enter the world by reading basmalah to be safe',
+  'Enter the world by reading precaution to be safe',
   'No need to beat around the bush, just stay on the gas and have fun',
 ];
 
--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,50 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { TitleText, TypingText } from '../components';
+import { staggerContainer, fadeIn } from '../utils/motion';
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| People on the World" textStyles="text-center" />
+      <TitleText
+        title={(
+          <>
+            Track friends around you and invite them to play together in the same world
+          </>
+        )}
+        textStyles="text-center"
+      />
+
+      <motion.div
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        className="relative mt-[69px] flex w-full h-[550px]"
+      >
+        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
+
+        {/* Here We are displaying chararcters on the map */}
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-01.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-[32%] left-[62%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-02.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-03.png" alt="people" className="w-full h-full" />
+        </div>
+
+        {/* Character on Map ends */}
+
+      </motion.div>
+    </motion.div>
   </section>
 );
 
