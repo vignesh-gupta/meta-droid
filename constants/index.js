72f1d4f623b3603dfd5eb497b5a41a007d649481
Vignesh Gupta <vighneshgupta32@gmail.com>
2022-12-10 17:18:46 +0530

    added insight section

--- constants/index.js
+++ constants/index.js
@@ -50,7 +50,7 @@ export const newFeatures = [
 export const insights = [
   {
     imgUrl: '/planet-06.png',
-    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    title: 'The launch of the Metaverse and be happy :)',
     subtitle:
         'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,34 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { insights } from '../constants';
+import { InsightCard, TitleText, TypingText } from '../components';
+import { staggerContainer } from '../utils/motion';
+
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText title="Insights about metaverse" textStyles="text-center" />
+
+      <div className="mt-[50px] flex flex-col gap-[30px]">
+        {insights.map((insight, i) => (
+          <InsightCard
+            key={`Insight-${i + 1}`}
+            {...insight}
+            index={i + 1}
+          />
+        ))}
+      </div>
+    </motion.div>
   </section>
 );
 
